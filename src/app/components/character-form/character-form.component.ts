8578df5fa971dc068744f0b9cfad35916c9eb43d
Garrett Vorbeck <garrett.vorbeck@lab49.com>
2024-07-31 15:12:52 -0400

    testing Stepper

--- src/app/components/character-form/character-form.component.ts
+++ src/app/components/character-form/character-form.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { Button } from 'primeng/button';
+import { StepperModule } from 'primeng/stepper';
 
 @Component({
   selector: 'app-character-form',
   standalone: true,
-  imports: [],
+  imports: [StepperModule, Button],
   templateUrl: './character-form.component.html',
-  styleUrl: './character-form.component.scss'
+  styleUrl: './character-form.component.scss',
 })
-export class CharacterFormComponent {
-
-}
+export class CharacterFormComponent {}
