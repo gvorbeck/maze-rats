af00b8155799adf12dda025d1cb728acc38fad0a
Garrett Vorbeck <garrett.vorbeck@lab49.com>
2024-07-31 14:49:09 -0400

    test button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
