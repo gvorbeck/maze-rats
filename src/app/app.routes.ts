3cffcce54d76693f79247792bc26ded059d562ff
Garrett Vorbeck <garrett.vorbeck@lab49.com>
2024-07-31 13:54:36 -0400

    refactor: Update imports in app.component.ts and app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CharacterListComponent } from './components/character-list/character-list.component';
+import { CharacterFormComponent } from './components/character-form/character-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/characters', pathMatch: 'full' },
+  { path: 'characters', component: CharacterListComponent },
+  { path: 'characters/new', component: CharacterFormComponent },
+];
