a37a8f57d75e1f5d0b3cc86f0523d42a3d7fb78b
Garrett Vorbeck <garrett.vorbeck@lab49.com>
2024-08-06 11:36:23 -0400

    Refactor character-form-items component to organize items by category and update instructions

--- src/app/components/character-form/character-form-items/character-form-items.component.ts
+++ src/app/components/character-form/character-form-items/character-form-items.component.ts
@@ -19,19 +19,10 @@ export class CharacterFormItemsComponent {
     backpack: InventoryItem[];
   }>();
 
-  availableItems: InventoryItem[] | undefined;
+  availableItems: InventoryItem[] = [];
   selectedItems: InventoryItem[] = [];
   itemsDisabled: boolean = false;
-  draggedItem: InventoryItem | undefined | null;
-  // maxItems: number = 6;
-  // availableLightWeapons: InventoryItem[] | undefined;
-  // availableHeavyWeapons: InventoryItem[] | undefined;
-  // availableRangedWeapons: InventoryItem[] | undefined;
-  // handsItems: InventoryItem[] = [];
-  // beltItems: InventoryItem[] = [];
-  // wornItems: InventoryItem[] = [];
-  // backpackItems: InventoryItem[] = [];
-  // draggedItem: InventoryItem | undefined | null;
+  draggedItem: InventoryItem | null = null;
 
   constructor(private inventoryService: InventoryService) {}
 
@@ -41,21 +32,6 @@ export class CharacterFormItemsComponent {
       .subscribe((data: InventoryItem[]) => {
         this.availableItems = data;
       });
-    // this.inventoryService
-    //   .getLightWeapons()
-    //   .subscribe((data: InventoryItem[]) => {
-    //     this.availableLightWeapons = data;
-    //   });
-    // this.inventoryService
-    //   .getHeavyWeapons()
-    //   .subscribe((data: InventoryItem[]) => {
-    //     this.availableHeavyWeapons = data;
-    //   });
-    // this.inventoryService
-    //   .getRangedWeapons()
-    //   .subscribe((data: InventoryItem[]) => {
-    //     this.availableRangedWeapons = data;
-    //   });
   }
 
   dragStart(item: InventoryItem) {
@@ -67,116 +43,38 @@ export class CharacterFormItemsComponent {
   }
 
   drop(refund?: boolean) {
-    if (!refund) {
-      if (this.draggedItem && !this.itemsDisabled) {
-        let draggedItemIndex = this.findIndex(this.draggedItem);
-        this.selectedItems = [
-          ...(this.selectedItems as InventoryItem[]),
+    if (this.draggedItem) {
+      if (!refund) {
+        if (!this.itemsDisabled) {
+          this.moveItem(
+            this.draggedItem,
+            this.availableItems,
+            this.selectedItems
+          );
+        }
+      } else {
+        this.moveItem(
           this.draggedItem,
-        ];
-        this.availableItems = this.availableItems?.filter(
-          (val, i) => i != draggedItemIndex
+          this.selectedItems,
+          this.availableItems
         );
-        this.draggedItem = null;
       }
-    } else {
-      // remove from selectedItems and add it back to availableItems
+      this.draggedItem = null;
+      this.isDisabled('items');
     }
-    this.isDisabled('items');
   }
 
-  findIndex(item: InventoryItem) {
-    let index = -1;
-    for (let i = 0; i < (this.availableItems as InventoryItem[]).length; i++) {
-      if (item.name === (this.availableItems as InventoryItem[])[i].name) {
-        index = i;
-        break;
-      }
+  moveItem(item: InventoryItem, from: InventoryItem[], to: InventoryItem[]) {
+    const index = from.findIndex((i) => i.name === item.name);
+    if (index !== -1) {
+      to.push(item);
+      from.splice(index, 1);
     }
-    return index;
   }
 
   isDisabled(source: 'items' | 'weapons') {
     if (source === 'items') {
       this.itemsDisabled = this.selectedItems.length >= 6;
-      console.log('itemsDisabled:', this.itemsDisabled);
     }
   }
-
-  // dragStart(item: InventoryItem) {
-  //   // this.draggedItem = item;
-  // }
-
-  // drop(area: string) {
-  //   // if (this.draggedItem && this.getTotalItems() < this.maxItems) {
-  //   //   let draggedItemIndex = this.findIndex(this.draggedItem);
-  //   //   switch (area) {
-  //   //     case 'hands':
-  //   //       this.handsItems.push(this.draggedItem);
-  //   //       break;
-  //   //     case 'belt':
-  //   //       this.beltItems.push(this.draggedItem);
-  //   //       break;
-  //   //     case 'worn':
-  //   //       this.wornItems.push(this.draggedItem);
-  //   //       break;
-  //   //     case 'backpack':
-  //   //       this.backpackItems.push(this.draggedItem);
-  //   //       break;
-  //   //   }
-  //   //   this.availableItems = this.availableItems?.filter(
-  //   //     (val, i) => i !== draggedItemIndex
-  //   //   );
-  //   //   this.itemsChanged.emit(this.getAllSelectedItems());
-  //   //   this.draggedItem = null;
-  //   // }
-  // }
-
-  // dragEnd() {
-  //   // this.draggedItem = null;
-  // }
-
-  // findIndex(item: InventoryItem) {
-  //   // let index = -1;
-  //   // for (let i = 0; i < (this.availableItems as InventoryItem[]).length; i++) {
-  //   //   if (item.name === (this.availableItems as InventoryItem[])[i].name) {
-  //   //     index = i;
-  //   //     break;
-  //   //   }
-  //   // }
-  //   // return index;
-  // }
-
-  // getAllSelectedItems(): {
-  // //   hands: InventoryItem[];
-  // //   belt: InventoryItem[];
-  // //   worn: InventoryItem[];
-  // //   backpack: InventoryItem[];
-  // // } {
-  // //   return {
-  // //     hands: this.handsItems,
-  // //     belt: this.beltItems,
-  // //     worn: this.wornItems,
-  // //     backpack: this.backpackItems,
-  // //   };
-  // }
-
-  // getTotalItems(): number {
-  //   // return (
-  //   //   this.handsItems.length +
-  //   //   this.beltItems.length +
-  //   //   this.wornItems.length +
-  //   //   this.backpackItems.length
-  //   // );
-  // }
-
-  // isDraggable(): boolean {
-  //   // return this.getTotalItems() < this.maxItems;
-  // }
-
-  // onItemReturn(item: InventoryItem) {
-  //   // if (!this.availableItems?.includes(item)) {
-  //   //   this.availableItems?.push(item);
-  //   // }
-  // }
 }
