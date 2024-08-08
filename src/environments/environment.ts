fcb51f84a9ac4fce675400276963c23dd2d78a1e
Garrett Vorbeck <gvorbeck@gmail.com>
2024-08-08 15:44:57 -0400

    supabase setup

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://whmnxsfxumvqqihmrmss.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndobW54c2Z4dW12cXFpaG1ybXNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjMxNDMxMzMsImV4cCI6MjAzODcxOTEzM30.tJy8pF2vt-Liire8RMbKRP0TVi884jVnhkHBAnaENhM',
+};
