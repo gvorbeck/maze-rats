6979bf96a9a87bcf39d5af66a7cd6de40d28d1db
Garrett Vorbeck <gvorbeck@gmail.com>
2024-08-08 16:06:27 -0400

    setup auth account components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
+import { SupabaseService } from './services/supabase.service';
+import { AccountComponent } from './components/account/account.component';
+import { AuthComponent } from './components/auth/auth.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    AccountComponent,
+    AuthComponent,
+    CommonModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'maze-rats';
+  session: any;
+
+  constructor(private readonly supabase: SupabaseService) {
+    this.session = this.supabase.session;
+  }
+
+  ngOnInit() {
+    this.supabase.authChanges((_, session) => (this.session = session));
+  }
 }
